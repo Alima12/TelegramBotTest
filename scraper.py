0dd51d08d2f59f9bfc5201e7ad99ff9382653b22
Alimah12 <aliali.ali1378@yahoo.com>
2022-04-21 00:17:48 +0430

    scraper created

--- scraper.py
+++ scraper.py
@@ -1,1 +1,35 @@
 from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+
+class Scraper:
+    sleep_time = 10
+    def __init__(self):
+        # browser = webdriver.Chrome(executable_path="driver/chromedriver.exe")
+        browser = webdriver.Firefox(executable_path="driver/geckodriver.exe")
+        time.sleep(2)
+
+        browser.get("http://www.tsetmc.com/Loader.aspx?ParTree=151312")
+        time.sleep(self.sleep_time)
+        self.scrapper = browser
+    
+    def update_price(self):
+        soup = BeautifulSoup(self.scrapper.page_source, "html.parser")
+        result = soup.select("#trade > div.objbox > table> tbody > tr")[1:10]
+        indexer = 0
+        records = []
+        for r in result:
+            _temp_record = {}
+            res = r.find_all("td")
+            _temp_record["Symbol"] = res[-1].text
+            _temp_record["FullName"] = res[-2].text
+            _temp_record["Type"] = indexer
+            _temp_record["LastPrice"] = res[11].text
+            _temp_record["ClosedPrice"] = res[8].text
+            indexer+=1
+            records.append(_temp_record)
+
+        self.scrapper.close()
+        return records
+
+        
